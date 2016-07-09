5dcb72e6ff5f846b4643f8dff5b2463cf39651fb
Park Ju Hyung <qkrwngud825@gmail.com>
2016-07-09 22:02:50 +0900

    op3: disable dexpreopt
    Gapps fucks this up anyways
    
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -142,15 +142,6 @@ OVERRIDE_RS_DRIVER:= libRSDriver_adreno.so
 MAX_VIRTUAL_DISPLAY_DIMENSION := 4096
 TARGET_FORCE_HWC_FOR_VIRTUAL_DISPLAYS := true
 
-# Enable dexpreopt to speed boot time
-ifeq ($(HOST_OS),linux)
-  ifeq ($(call match-word-in-list,$(TARGET_BUILD_VARIANT),user),true)
-    ifeq ($(WITH_DEXPREOPT),)
-      WITH_DEXPREOPT := true
-    endif
-  endif
-endif
-
 # GPS
 TARGET_NO_RPC := true
 USE_DEVICE_SPECIFIC_GPS := true
